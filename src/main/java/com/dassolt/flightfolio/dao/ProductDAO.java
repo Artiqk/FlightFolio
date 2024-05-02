a7763742209422147acddf374f0d9f75e9c5e068
Antoine A. <antoineabonneau94@gmail.com>
2024-05-02 16:15:49 +0200

    fixed add method

--- src/main/java/com/dassolt/flightfolio/dao/ProductDAO.java
+++ src/main/java/com/dassolt/flightfolio/dao/ProductDAO.java
@@ -3,10 +3,7 @@ package com.dassolt.flightfolio.dao;
 import com.dassolt.flightfolio.model.Product;
 import com.dassolt.flightfolio.util.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.List;
 
 public class ProductDAO implements GenericDAO<Product> {
@@ -22,25 +19,47 @@ public class ProductDAO implements GenericDAO<Product> {
 
     @Override
     public void add(Product product) {
-        String query = "INSERT INTO product(id,name,description,price,quantity,engine_nb,seat_nb,wingspan,length,service_ceiling,can_spread_democracy,manufacturer_id,engine_manufacturer_id,category_id) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        String query = "INSERT INTO product(name,description,price,quantity,engine_nb,seat_nb,wingspan,length,service_ceiling,can_spread_democracy,manufacturer_id,engine_manufacturer_id,category_id) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
         try (PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setInt(1, product.getId());
-            stmt.setString(2, product.getName());
-            stmt.setString(3, product.getDescription());
-            stmt.setDouble(4, product.getPrice());
-            stmt.setInt(5, product.getQuantity());
-            stmt.setInt(6, product.getEngineNb());
-            stmt.setInt(7, product.getSeatNb());
-            stmt.setDouble(8, product.getWingspan());
-            stmt.setDouble(9, product.getLength());
-            stmt.setInt(10, product.getServiceCeiling());
-            stmt.setBoolean(11, product.isCanSpreadDemocracy());
-            stmt.setInt(12, product.getManufacturerId());
-            stmt.setInt(13, product.getEngineManufacturerId());
-            stmt.setInt(14, product.getCategoryId());
+            conn.setAutoCommit(false);
+
+            stmt.setString(1, product.getName());
+            stmt.setString(2, product.getDescription());
+            stmt.setDouble(3, product.getPrice());
+            stmt.setInt(4, product.getQuantity());
+            stmt.setInt(5, product.getEngineNb());
+            stmt.setInt(6, product.getSeatNb());
+            stmt.setDouble(7, product.getWingspan());
+            stmt.setDouble(8, product.getLength());
+            stmt.setInt(9, product.getServiceCeiling());
+            stmt.setBoolean(10, product.canSpreadDemocracy());
+            stmt.setInt(11, product.getManufacturerId());
+            stmt.setInt(12, product.getEngineManufacturerId());
+            stmt.setInt(13, product.getCategoryId());
+
             stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            if (rs.next()) {
+                product.setId(rs.getInt(1));
+            }
+
+            conn.commit();
         } catch (SQLException e) {
+            try {
+                if (!(conn == null || conn.isClosed())) conn.rollback();
+            } catch (SQLException ex) {
+                System.out.println("ERROR: Failed to rollback transaction - " + ex.getMessage());
+            }
             System.out.println("ERROR: Failed to add product to the database - " + e.getMessage());
+        } finally {
+            try {
+                if (!(conn == null || conn.isClosed())) conn.setAutoCommit(true);
+            } catch (SQLException e) {
+                System.out.println("ERROR: Could not reset auto-commit - " + e.getMessage());
+            }
         }
     }
 
