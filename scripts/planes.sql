37b4a05417e58279f3ae7afb76fda7a8bfd1e7dd
Antoine A. <antoineabonneau94@gmail.com>
2024-05-02 19:30:50 +0200

    fixed category id

--- scripts/planes.sql
+++ scripts/planes.sql
@@ -75,6 +75,6 @@ INSERT INTO `product` (
 ('Boeing 777X', 'The newest member of the world-renowned Boeing 777 family, designed for efficiency and exceptional passenger comfort.', 320000000.00, 5, 1, 2, 1, 396, 64.80, 73.90, 13100, 0, 1),
 ('Boeing 747 - The Queen of the Skies', 'An iconic jumbo jet that revolutionized air travel with its size and range.', 387000000.00, 4, 1, 4, 1, 416, 68.40, 70.60, 13100, 0, 1),
 ('Airbus A321 Neo', 'A market-leading narrow-body jetliner that blends fuel efficiency with comfort and performance.', 129000000.00, 7, 2, 2, 4, 244, 35.80, 44.50, 12000, 0, 1),
-('F/A-18E Super Hornet', 'A highly capable, twin-engine, multirole fighter known for its agility and stealth.', 70000000.00, 8, 3, 2, 3, 1, 13.62, 18.31, 15000, 1, 3),
+('F/A-18E Super Hornet', 'A highly capable, twin-engine, multirole fighter known for its agility and stealth.', 70000000.00, 8, 3, 2, 3, 1, 13.62, 18.31, 15000, 1, 1),
 ('F-16C Block 70 Viper', 'A 4th generation multi-role fighter aircraft, known for its versatility and advanced avionics.', 64000000.00, 12, 3, 1, 3, 1, 9.96, 15.06, 15000, 1, 3),
 ('Rafale M', 'A powerful and agile multirole fighter, equipped to operate from aircraft carriers and meet various mission requirements.', 74000000.00, 9, 4, 1, 4, 1, 10.90, 15.27, 15235, 1, 3);
