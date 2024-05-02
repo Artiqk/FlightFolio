9ba3c99cb7c2e91434e528737dc04a709c9d64f9
Antoine A. <antoineabonneau94@gmail.com>
2024-05-02 23:01:25 +0200

    ONE SHOT

--- src/test/java/com/dassolt/flightfolio/dao/ManufacturerDAOTest.java
+++ src/test/java/com/dassolt/flightfolio/dao/ManufacturerDAOTest.java
@@ -1,4 +1,35 @@
 package com.dassolt.flightfolio.dao;
 
+import com.dassolt.flightfolio.model.Manufacturer;
+
 public class ManufacturerDAOTest {
+    public static void main(String[] args) {
+        Manufacturer lockheedMartin = new Manufacturer("Lockheed Martin");
+        Manufacturer dassault = new Manufacturer("Dassault");
+
+        try {
+            ManufacturerDAO dao = new ManufacturerDAO();
+
+            dao.add(lockheedMartin);
+            dao.add(dassault);
+
+             assert lockheedMartin.getId().equals(dao.findById(lockheedMartin.getId()).getId());
+             assert dao.findAll().size() == 2;
+
+             lockheedMartin.setName("Lockhead Martin");
+             dao.update(lockheedMartin);
+
+             assert lockheedMartin.getName().equals(dao.findById(lockheedMartin.getId()).getName());
+
+             dao.delete(lockheedMartin);
+
+             assert dao.findById(lockheedMartin.getId()) == null;
+
+             dao.delete(dassault);
+
+             assert dao.findAll().isEmpty();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
