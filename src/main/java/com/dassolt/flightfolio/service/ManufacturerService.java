02eda211416f7f2d32a796994d4959966c8c5463
Antoine A. <antoineabonneau94@gmail.com>
2024-05-03 11:20:14 +0200

    finished methods

--- src/main/java/com/dassolt/flightfolio/service/ManufacturerService.java
+++ src/main/java/com/dassolt/flightfolio/service/ManufacturerService.java
@@ -1,4 +1,64 @@
 package com.dassolt.flightfolio.service;
 
+import com.dassolt.flightfolio.dao.ManufacturerDAO;
+import com.dassolt.flightfolio.model.Manufacturer;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ManufacturerService {
+    private ManufacturerDAO manufacturerDAO;
+
+    public ManufacturerService() throws SQLException {
+        manufacturerDAO = new ManufacturerDAO();
+    }
+
+    public void addManufacturer(Manufacturer manufacturer) {
+        try {
+            manufacturerDAO.add(manufacturer);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to add manufacturer due to a database error", e);
+        }
+    }
+
+    public Manufacturer retrieveManufacturerById(String id) {
+        Manufacturer manufacturer = null;
+
+        try {
+            manufacturer = manufacturerDAO.findById(id);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to retrieve manufacturer due to a database error", e);
+        }
+
+        return manufacturer;
+    }
+
+    public List<Manufacturer> retrieveAllManufacturers() {
+        List<Manufacturer> manufacturers = null;
+
+        try {
+            manufacturers = manufacturerDAO.findAll();
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to retrieve all manufacturers due to a database error", e);
+        }
+
+        return manufacturers;
+    }
+
+    public void updateManufacturer(Manufacturer manufacturer) {
+        try {
+            manufacturerDAO.update(manufacturer);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to update manufacturer due to a database error", e);
+        }
+    }
+
+    public void deleteManufacturer(Manufacturer manufacturer) {
+        try {
+            manufacturerDAO.delete(manufacturer);
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to delete manufacturer due to a database error", e);
+        }
+    }
 }
